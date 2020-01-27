b3f4c2eba70d32f4bcf4221e170d72292416b26f
Salman Rahman Auvi <36760223+salman132@users.noreply.github.com>
2020-01-27 11:57:39 +0600

    Update storeCode.php

--- storeCode.php
+++ storeCode.php
@@ -13,6 +13,36 @@ $checkData = MeetingRoomBooking::where('room_id', $room->room_no)->where('status
 
 // Find busy aganets between 2 dates
 
+  public function ajax(Request $request, $id){
+    if($request->ajax()){
+        $startTime = $id; //Getting Starting Date
+        $endTime = $request->end_date;
+
+
+        $busy = array(); //Agents Who are really busy
+
+        // Array Covert to Single Dimension
+
+        function array_flatten($array) {
+            if (!is_array($array)) {
+                return FALSE;
+            }
+            $result = array();
+            foreach ($array as $key => $value) {
+                if (is_array($value)) {
+                    $result = array_merge($result, array_flatten($value));
+                }
+                else {
+                    $result[$key] = $value;
+                }
+            }
+            return $result;
+        }
+
+
+        //End
+
+
         $assigned_agent = AssignAgentToProject::where(function ($dateQuery) use ($startTime, $endTime){
 
             $dateQuery->where(function ($query) use ($startTime, $endTime) {
@@ -27,6 +57,18 @@ $checkData = MeetingRoomBooking::where('room_id', $room->room_no)->where('status
                     { $q->where('service_start','>',$startTime)->where('service_ends','<',$endTime); })
                     ->orWhere(function ($q) use ($startTime, $endTime){ $q->where('service_start','<=',$endTime)->where('service_ends','>',$endTime); });});
         })->get();
+
+
+        foreach ($assigned_agent as $key => $agent){
+            $busy[] = explode(",",$agent->agent_id);
+        }
+
+            //Getting Free Agents
+        $agents = Employee::where('role_id',6)->where('status','active')->whereNotIN('id',array_flatten($busy))->get();
+
+        return view('admin.roaster.assign.ajax.agents',compact('agents'))->render();
+    }
+}
 
 
 
